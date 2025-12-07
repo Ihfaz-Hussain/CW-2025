a62e71125797a12472c50f7ca268be7f665719e0
Ihfaz Hussain <chowdhuryihfaz@gmail.com>
2025-12-07 15:51:23 +0800

    Added javadoc

---
+++ CW2025-master/src/main/java/com/comp2042/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * The root package for the Tetris game application.
+ * <p>
+ * This package contains the main entry point class {@link com.comp2042.Main}
+ * which
+ * initializes the JavaFX application and loads the main menu.
+ * </p>
+ */
+package com.comp2042;
